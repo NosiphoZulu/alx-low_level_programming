6fd9172f7f2d984193ac62478d9558394da622bc
NosiphoZulu <nosiphozulu08@gmail.com>
2023-09-11 12:07:49 -0700

    Alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Returns: 0(success)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; 'z' ; i++)
+		putchar(i);
+	for (i = 'A' ; i <= 'Z'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}

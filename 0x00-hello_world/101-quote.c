291e48f9e424fc5637cb4c0d4aaee3fa0312751a
NosiphoZulu <nosiphozulu08@gmail.com>
2023-09-10 17:05:06 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <stdio>
-#include <unistd.h>
 
 /**
  * main - main function
@@ -8,5 +7,5 @@
 int main(void)
 {
 	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+return (1);
 }

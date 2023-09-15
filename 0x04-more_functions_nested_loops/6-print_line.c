351afe64d838e2b31d6a7988791605225f17da37
NosiphoZulu <nosiphozulu08@gmail.com>
2023-09-15 12:38:53 -0700

    Line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,9 +7,18 @@
 
 void print_line(void)
 {
-	if(n <= 0)
+	if (n <= 0)
 	{
 		_putchar('\n');
 	}
 	else
-		
+	{
+		int i;
+
+		for (i == 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}

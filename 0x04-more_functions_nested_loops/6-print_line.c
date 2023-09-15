6ad4411b83aab3cecbdcddcc1286ac676b8b7e9e
NosiphoZulu <nosiphozulu08@gmail.com>
2023-09-15 11:56:17 -0700

    Numbers

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: number of times the character should be printed
+ */
+
+void print_line(void)
+{
+	if(n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+		

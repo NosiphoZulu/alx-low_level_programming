efe14ef57acfb85f373a5b2668d79dc3bb8f4314
NosiphoZulu <nosiphozulu08@gmail.com>
2023-09-18 16:55:27 -0700

    Reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - print in reverse
+ * @s: string
+ * Return: 0
+ */
+
+void print_rev(char *s)
+{
+	int long1 = 0;
+	int o;
+
+	while (*s != '\0')
+	{
+		long1++;
+		s++;
+	}
+	s--;
+	for (o = long1; o > 0; o--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
